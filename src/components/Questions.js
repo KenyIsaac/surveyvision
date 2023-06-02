c4f6331adf59de12aa71e24ab9932c2c6a1a9f26
Keny Isaac <keny.isaac.lm@hotmail.com>
2023-06-02 13:23:01 -0500

    added

---
+++ src/components/Questions.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Questions() {
+  return (
+    <div>
+     <h2>Pregunta 1</h2>
+      <p>Texto de la pregunta 1</p>
+
+      <h2>Pregunta 2</h2>
+      <p>Texto de la pregunta 2</p>
+
+      {/* Agrega más preguntas aquí */}
+    </div>
+  );
+}
+
+export default Questions;
